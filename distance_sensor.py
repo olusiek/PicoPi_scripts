04d8a3ff5fd0a4fd6727e654b6932cd6e4798d33
Pawel Tanas <pawel.tanas@gmail.com>
2022-12-04 21:30:56 +0100

    First commit

---
+++ distance_sensor.py
@@ -0,0 +1,3 @@
+# this file will consists the code needed for a distance meter with all the details about wiring and networking
+# Assumption is to have a native IoT device which will connect and store information about the distance in dediated db (InfluxDB)
+# this device will be used to monitor the level of water in septic tank

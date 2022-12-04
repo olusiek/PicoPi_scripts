23652ddf65ee00a6ebd4454753212a04ba6abea7
Pawel Tanas <pawel.tanas@gmail.com>
2022-12-04 21:31:04 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a temporary project to collect all the usuable functions/hints to program Rpi Pico Pi W :)
+
+Enjoy!
---
+++ picoPi_templates.py
@@ -0,0 +1,29 @@
+#some pico pi basics
+
+
+#setting up the LED
+
+import machine
+
+led = machine.Pin("LED",machine.Pin.OUT)
+
+led.on()
+time.sleep(10)
+led.off()
+led.toggle()
+
+
+# setting up the WiFi
+
+import network
+import time
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect('ssid', 'pass')
+
+while not wlan.isconnected() and wlan.status() >= 0:
+    print("Waiting to connect:")
+    time.sleep(1)
+
+print(wlan.ifconfig())
